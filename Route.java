1aa622baa4e4e2c7fe15305f68a84e4687f2560d
Kylian LucasThuillier <kylian.lucast@gmail.com>
2024-06-11 15:08:30 +0200

    Route Fonctionnel

--- Route.java
+++ Route.java
@@ -1,29 +1,30 @@
 public class Route {
 
 	private int  nbSections;
-	private Mine mineDepart;
-	private Mine mineArriver;
+	private Sommet sommetDepart;
+	private Sommet sommetArriver;
 
 	// Fabrique de Route
-	public static Route creerRoute(int nbSections, Mine mineDepart, Mine mineArriver)
+	public static Route creerRoute(int nbSections, Sommet sommetDepart, Sommet sommetArriver)
 	{
-		if(nbSections <= 1 || nbSections >= 2) { return null; }
-		else if(mineDepart  == null)           { return null; }
-		else if(mineArriver == null)           { return null; }
-		else { return new Route(nbTroncons, villeDepart, villeArriver); }
+		if(nbSections <= 1 || nbSections >= 2)  { return null; }
+		else if(sommetDepart  == sommetArriver) { return null; }
+		else if(sommetDepart  == null)          { return null; }
+		else if(sommetArriver == null)          { return null; }
+		else { return new Route(nbSections, sommetDepart, sommetArriver); }
 	}
 
 
-	private Route(int nbSections, Mine mineDepart, Mine mineArriver)
+	private Route(int nbSections, Sommet sommetDepart, Sommet sommetArriver)
 	{
-		this.nbSections   = nbSections;
-		this.mineDepart  = mineDepart;
-		this.mineArriver = mineArriver;
+		this.nbSections    = nbSections;
+		this.sommetDepart  = sommetDepart;
+		this.sommetArriver = sommetArriver;
 
 	}
 
 	// Accesseurs
-	public int  getNbTroncons()  { return this.nbSections;  }
-	public Mine getMineDepart()  { return this.mineDepart;  }
-	public Mine getMineArriver() { return this.mineArriver; }
+	public int    getNbSelections() { return this.nbSections;    }
+	public Sommet getMineDepart()   { return this.sommetDepart;  }
+	public Sommet getMineArriver()  { return this.sommetArriver; }
 }
