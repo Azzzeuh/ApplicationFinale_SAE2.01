20b1fb48a60ec8a534c902e90c2253985abf19d8
Ladji Dianissy <ladji.dianissy@gmail.com>
2024-06-12 09:23:05 +0200

    joueur,plateau

--- Joueur.java
+++ Joueur.java
@@ -1,30 +1,36 @@
-public class Joueur
-{
+public class Joueur {
 
 	private static int numJoueur;
 	private int score;
 
 	private PlateauIndividuel plateauJoueur;
 
-
-	public Joueur()
-	{
+	public Joueur() {
 		this.numJoueur++;
 		this.score = 0;
 
 		this.plateauJoueur = null;
 	}
 
-	public void creerPlateauJoueur()
-	{
+	public void creerPlateauJoueur() {
 		this.plateauJoueur = new PlateauIndividuel();
 	}
 
 	// Setter
-	public void              setScore(int score)         { this.score = score; }
+	public void setScore(int score) {
+		this.score = score;
+	}
 
 	// Acceseur
-	public int               getNumJoueur()     { return this.numJoueur;     }
-	public Plateauindividuel getPlateauJoueur() { return this.plateauJoueur; }
-	public int               getScore()         { return this.score;         }
+	public int getNumJoueur() {
+		return this.numJoueur;
+	}
+
+	public PlateauIndividuel getPlateauJoueur() {
+		return this.plateauJoueur;
+	}
+
+	public int getScore() {
+		return this.score;
+	}
 }
