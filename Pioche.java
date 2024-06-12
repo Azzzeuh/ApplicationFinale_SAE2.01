31bb5bf2f178956120fe6d4425d0af19264e7ccc
Ladji Dianissy <ladji.dianissy@gmail.com>
2024-06-12 08:51:27 +0200

    oui

--- Pioche.java
+++ Pioche.java
@@ -9,25 +9,20 @@ public class Pioche {
 		this.initPioche();
 	}
 
-	public Jeton tirerJeton() 
-	{
-		if (this.jetons.isEmpty()) 
-		{
+	public Jeton tirerJeton() {
+		if (this.jetons.isEmpty()) {
 			return null;
-		} 
-		else 
-		{
+		} else {
 			return this.jetons.remove(0);
 		}
 	}
 
-	private void initPioche()
-	{
-		for(int cpt=0; cpt<8; cpt++)
-		{ jetons.add(new Jeton(JetonRessource.Nr)); }
-	
-		for(int cpt=0; cpt<4; cpt++)
-		{ 
+	private void initPioche() {
+		for (int cpt = 0; cpt < 8; cpt++) {
+			jetons.add(new Jeton(JetonRessource.Nr));
+		}
+
+		for (int cpt = 0; cpt < 4; cpt++) {
 			jetons.add(new Jeton(JetonRessource.Al));
 			jetons.add(new Jeton(JetonRessource.Ag));
 			jetons.add(new Jeton(JetonRessource.Au));
@@ -35,11 +30,10 @@ public class Pioche {
 			jetons.add(new Jeton(JetonRessource.Fe));
 			jetons.add(new Jeton(JetonRessource.Ni));
 			jetons.add(new Jeton(JetonRessource.Pt));
-			jetons.add(new Jeton(JetonRessource.Ti)); 
+			jetons.add(new Jeton(JetonRessource.Ti));
 		}
 
 		Collections.shuffle(jetons);
 	}
 
-
-
+}
