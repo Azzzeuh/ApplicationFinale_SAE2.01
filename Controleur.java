1dfa43bb9eafac2fb0b6d6a508cc3798f12e79d5
Kylian LucasThuillier <kylian.lucast@gmail.com>
2024-06-13 09:44:32 +0200

    derniére modif

---
+++ Controleur.java
@@ -0,0 +1,14 @@
+package application;
+
+import metier.*;
+import ihm.*;
+
+public class Controleur
+{
+	public static void main(String[] a)
+	{
+		FrameMenu menu = new FrameMenu(100, 100);
+
+	}
+
+}
