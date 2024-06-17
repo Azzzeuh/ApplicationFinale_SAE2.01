5a59e9c0c757f12393a509e4e030c5f5ad895ea9
Mat <sefilmatys6@gmail.com>
2024-06-17 10:33:32 +0200

    Controleur modif

--- Controleur.java
+++ Controleur.java
@@ -3,13 +3,50 @@ import ihm.*;
 
 public class Controleur
 {
+
+		// Arraylist
+		private ArrayList<Sommet> listSommet;
+		private ArrayList<Route>  listRoute;
+
 	public Controleur()
 	{
 		FramePlateau plt = new FramePlateau(100, 100);
+
+		this.listSommet = new ArrayList<>();
+		this.listRoute = new ArrayList<>();
 	}
+
+	
+
+	// Vider les listes
+	public ArrayList<Route> viderListeRoute(ArrayList<Route> liste)
+	{
+		for(Route r : liste)
+		{
+			liste.remove(r);
+		}
+		return liste;
+	}
+
+	/*public ArrayList<Sommet> viderListeSommet(ArrayList<Sommet> liste)
+	{
+		for(Sommet s : liste)
+		{
+			liste.remove(s);
+			Sommet.resetNbSommet();
+		}
+		return liste;
+
+		public ArrayList<Sommet> getSommetList() { return this.listSommet; }
+		public ArrayList<Route>  getRouteList()  { return this.listRoute;  }
+
+	}*/
+
+
 	public static void main(String[] a)
 	{
 		new Controleur();
 	}
 
 }
+
